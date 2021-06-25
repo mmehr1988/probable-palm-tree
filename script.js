85b8467185ed99717ad0246d7a7f11935464728e
Mehdi Mehrabani <mmehr1988@gmail.com>
2021-06-25 15:56:44 -0400

    updated length check to account for non numbers and empty and when cancel is pressed

--- script.js
+++ script.js
@@ -15,39 +15,59 @@ function writePassword() {
 generateBtn.addEventListener('click', writePassword);
 
 // --------------------------------------------------------------------------------------
-// CHARACTER VAR LIST -------------------------------------------------------------------
+// CHARACTER VAR LIST
 // --------------------------------------------------------------------------------------
 
 var numbersList = '0123456789';
 var lowersList = 'abcdefghijklmnopqrstuvwxyz';
 var uppersList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var symbolsList = '!"#$%&()*+,-./:;<=>?@[]^_`{|}~';
+var lengthMessage = 'Enter a number from 8 to 128 for password length.';
 
 // --------------------------------------------------------------------------------------
-// PROMPT BOX + CONFIRM BOX + FUNCTION --------------------------------------------------
+// FUNCTION: RANDOMIZER
+// --------------------------------------------------------------------------------------
+
+function randomizer(i) {
+  return `${i.charAt(Math.random() * i.length)}`;
+}
+
+// --------------------------------------------------------------------------------------
+// MAIN FUNCTION: PROMPT BOX + CONFIRM BOX + FUNCTION
 // --------------------------------------------------------------------------------------
 
 function generatePassword() {
   var masterCharList = '';
 
-  // After each confirmed character, 1 character from that list is reserved in the final password
+  // RESERVED PASSWORD LIST FOR EACH CONFIRMED CHARACTER TYPE
   var passwordConfirmList = '';
 
-  // TRACKER | AT LEAST 1 TYPE --------------------------------------
+  // TRACKER | AT LEAST 1 TYPE
   var trackerConfirm = [];
 
-  // LENGTH PROMPT + CHECK FOR CHARACTER LENGTH ---------------------
-  var lengthPrompt = prompt('Enter a number from 8 to 128 for password length.');
+  // PROMPT BOX: LENGTH + CHECK FOR CHARACTER LENGTH
+  var lengthPrompt = prompt(lengthMessage);
 
-  if (lengthPrompt < 8) {
-    alert('💔  Try Again! Too short. 💔 \r\n Password length needs to be between 8 & 128.');
-    return '';
-  } else if (lengthPrompt > 128) {
-    alert('💔  Try Again! Too long. 💔 \r\n Password length needs to be between 8 & 128.');
-    return '';
+  // While loop as we do not know how many times user might get the it wrong.
+  while (lengthPrompt == null || !lengthPrompt || lengthPrompt < 8 || lengthPrompt > 128 || !Number(lengthPrompt)) {
+    if (lengthPrompt == null) {
+      return '';
+    } else if (!lengthPrompt) {
+      alert('🚫 Did you forget to type a number?');
+      lengthPrompt = prompt(lengthMessage);
+    } else if (lengthPrompt > 128) {
+      alert('💔  Try Again! Too long. 💔 \r\n Password length needs to be between 8 & 128.');
+      lengthPrompt = prompt(lengthMessage);
+    } else if (lengthPrompt < 8) {
+      alert('💔  Try Again! Too short. 💔 \r\n Password length needs to be between 8 & 128.');
+      lengthPrompt = prompt(lengthMessage);
+    } else if (!Number(lengthPrompt)) {
+      alert('🚫 You did not type a number. You typed >>>>>>>> ' + lengthPrompt);
+      lengthPrompt = prompt(lengthMessage);
+    }
   }
 
-  // CONFIRM CHARACTER BOX + PUSH FOR ANY CANCELS TO TRACKER --------
+  // CONFIRM BOX: FOR SYMBOLS TO INCLUDE
   var numbersPrompt = confirm('Do you want to include numbers?');
   if (numbersPrompt) {
     masterCharList += numbersList;
@@ -80,7 +100,7 @@ function generatePassword() {
     trackerConfirm.push(symbolsPrompt);
   }
 
-  // CHECK | AT LEAST 1 TYPE ----------------------------------------
+  // CONFIRM BOX: CHECK IF AT LEAST 1 SYMBOL TYPE CHOSEN
   if (trackerConfirm.length === 4) {
     alert('💔  Try Again! 💔 \r\n At least 1 type needs to be selected.');
     return '';
@@ -91,12 +111,4 @@ function generatePassword() {
     password += masterCharList[Math.floor(Math.random() * masterCharList.length)];
   }
   return password + passwordConfirmList;
-}
-
-// --------------------------------------------------------------------------------------
-// RANDOMIZER ---------------------------------------------------------------------------
-// --------------------------------------------------------------------------------------
-
-function randomizer(i) {
-  return `${i.charAt(Math.random() * i.length)}`;
 }
